689c438fd36c146c4c77922033e153d9b944e40d
vigbav36 <90998381+vigbav36@users.noreply.github.com>
2023-04-21 17:44:01 +0530

    inital commit

---
+++ draw.js
@@ -0,0 +1,27 @@
+
+function check(point, polygon) {
+    const n = polygon.length
+    let isIn = false
+    const x = point[0]
+    const y = point[1]
+    let x1, x2, y1, y2
+
+    x1 = polygon[n-1][0]
+    y1 = polygon[n-1][1]
+
+    for (let i = 0; i < n; ++i) {
+        x2 = polygon[i][0];
+        y2 = polygon[i][1];
+
+        if (y < y1 !== y < y2 && x < (x2 - x1) * (y - y1) / (y2 - y1) + x1) {
+            isIn = !isIn;
+        }
+        x1 = x2
+        y1 = y2
+    }
+
+    return isIn;
+}
+
+
+
